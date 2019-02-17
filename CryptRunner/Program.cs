8d5a843922e35d876b946dd952b69818cc2f5673
GrahamBurri <36557278+GrahamBurri@users.noreply.github.com>
2019-02-16 23:03:35 -0500

    Version 0.1
    VS solution and project files created, character system created and tested

---
+++ CryptRunner/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CryptLib;
+
+namespace CryptRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Cuneiform c = new Cuneiform();
+            Console.WriteLine(Cuneiform.getCuneiformNumber(1));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CuneiformCrypt/CryptLib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CryptLib
+{
+    public class Cuneiform
+    {
+        private const int ONE = 0x12415;
+
+        public static string getCuneiformNumber(int decimalNumber)
+        {
+            return Char.ConvertFromUtf32(ONE);
+        }
+    }
+}
